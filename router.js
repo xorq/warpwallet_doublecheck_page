87e051ae9e2377a1084b9fa8374c7aa95f58b05d
xorq <xorque@gmail.com>
2016-07-20 17:47:28 +0700

    cleaning

--- router.js
+++ router.js
@@ -2,24 +2,14 @@ define([
   'jquery',
   'underscore',
   'backbone',
-  'views/index',
   'views/vault'
-], function($, _, Backbone, IndexView, VaultView){
+], function($, _, Backbone, VaultView){
 
   var AppRouter = Backbone.Router.extend({
     routes: {
-      '': 'index',
       'vault' : 'vault',
     },
     currentView: false,
-    index: function() {
-      if (this.currentView) {
-        this.currentView.undelegateEvents();
-      }
-      this.currentView = new IndexView({ model: new Transaction });
-      this.currentView.render();
-      $('.nav > li').removeClass('active').filter('[name=create]').addClass('active');
-    },
     vault: function() {
       if (this.currentView) {
         this.currentView.undelegateEvents();
